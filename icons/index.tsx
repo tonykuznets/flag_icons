50433ae7815e74e1055db91ea3dfe0ae578e7fab
Anton Kuznetsov <anantonio.kuzentsov@gmail.com>
2023-06-29 10:12:10 +0400

    update icons

---
+++ icons/index.tsx
@@ -0,0 +1,513 @@
+import * as ad from "./ad.svg";
+import * as ae from "./ae.svg";
+import * as af from "./af.svg";
+import * as ag from "./ag.svg";
+import * as ai from "./ai.svg";
+import * as al from "./al.svg";
+import * as am from "./am.svg";
+import * as ao from "./ao.svg";
+import * as aq from "./aq.svg";
+import * as ar from "./ar.svg";
+import * as as from "./as.svg";
+import * as at from "./at.svg";
+import * as au from "./au.svg";
+import * as aw from "./aw.svg";
+import * as ax from "./ax.svg";
+import * as az from "./az.svg";
+import * as ba from "./ba.svg";
+import * as bb from "./bb.svg";
+import * as bd from "./bd.svg";
+import * as be from "./be.svg";
+import * as bf from "./bf.svg";
+import * as bg from "./bg.svg";
+import * as bh from "./bh.svg";
+import * as bi from "./bi.svg";
+import * as bj from "./bj.svg";
+import * as bl from "./bl.svg";
+import * as bm from "./bm.svg";
+import * as bn from "./bn.svg";
+import * as bq from "./bq.svg";
+import * as bqbo from "./bq-bo.svg";
+import * as bo from "./bo.svg";
+import * as bqsa from "./bq-sa.svg";
+import * as bqse from "./bq-se.svg";
+import * as br from "./br.svg";
+import * as bs from "./bs.svg";
+import * as bt from "./bt.svg";
+import * as bv from "./bv.svg";
+import * as bw from "./bw.svg";
+import * as by from "./by.svg";
+import * as bz from "./bz.svg";
+import * as ca from "./ca.svg";
+import * as cc from "./cc.svg";
+import * as cd from "./cd.svg";
+import * as cf from "./cf.svg";
+import * as cg from "./cg.svg";
+import * as ch from "./ch.svg";
+import * as ci from "./ci.svg";
+import * as ck from "./ck.svg";
+import * as cl from "./cl.svg";
+import * as cm from "./cm.svg";
+import * as cn from "./cn.svg";
+import * as co from "./co.svg";
+import * as cr from "./cr.svg";
+import * as cu from "./cu.svg";
+import * as cv from "./cv.svg";
+import * as cw from "./cw.svg";
+import * as cx from "./cx.svg";
+import * as cy from "./cy.svg";
+import * as cz from "./cz.svg";
+import * as de from "./de.svg";
+import * as dj from "./dj.svg";
+import * as dk from "./dk.svg";
+import * as dm from "./dm.svg";
+import * as DO from "./do.svg";
+import * as dz from "./dz.svg";
+import * as ec from "./ec.svg";
+import * as ee from "./ee.svg";
+import * as eg from "./eg.svg";
+import * as eh from "./eh.svg";
+import * as er from "./er.svg";
+import * as es from "./es.svg";
+import * as et from "./et.svg";
+import * as fi from "./fi.svg";
+import * as fj from "./fj.svg";
+import * as fk from "./fk.svg";
+import * as fm from "./fm.svg";
+import * as fo from "./fo.svg";
+import * as fr from "./fr.svg";
+import * as ga from "./ga.svg";
+import * as gb from "./gb.svg";
+import * as gbeng from "./gb-eng.svg";
+import * as gbnir from "./gb-nir.svg";
+import * as gbsct from "./gb-sct.svg";
+import * as gbukm from "./gb-ukm.svg";
+import * as gbwls from "./gb-wls.svg";
+import * as gd from "./gd.svg";
+import * as ge from "./ge.svg";
+import * as gf from "./gf.svg";
+import * as gg from "./gg.svg";
+import * as gh from "./gh.svg";
+import * as gi from "./gi.svg";
+import * as gl from "./gl.svg";
+import * as gm from "./gm.svg";
+import * as gn from "./gn.svg";
+import * as gp from "./gp.svg";
+import * as gq from "./gq.svg";
+import * as gr from "./gr.svg";
+import * as gs from "./gs.svg";
+import * as gt from "./gt.svg";
+import * as gu from "./gu.svg";
+import * as gw from "./gw.svg";
+import * as gy from "./gy.svg";
+import * as hk from "./hk.svg";
+import * as hm from "./hm.svg";
+import * as hn from "./hn.svg";
+import * as hr from "./hr.svg";
+import * as ht from "./ht.svg";
+import * as hu from "./hu.svg";
+import * as id from "./id.svg";
+import * as ie from "./ie.svg";
+import * as il from "./il.svg";
+import * as im from "./im.svg";
+import * as inFlag from "./in.svg";
+import * as io from "./io.svg";
+import * as iq from "./iq.svg";
+import * as ir from "./ir.svg";
+import * as is from "./is.svg";
+import * as it from "./it.svg";
+import * as je from "./je.svg";
+import * as jm from "./jm.svg";
+import * as jo from "./jo.svg";
+import * as jp from "./jp.svg";
+import * as ke from "./ke.svg";
+import * as kg from "./kg.svg";
+import * as kh from "./kh.svg";
+import * as ki from "./ki.svg";
+import * as km from "./km.svg";
+import * as kn from "./kn.svg";
+import * as kp from "./kp.svg";
+import * as kr from "./kr.svg";
+import * as kw from "./kw.svg";
+import * as ky from "./ky.svg";
+import * as kz from "./kz.svg";
+import * as la from "./la.svg";
+import * as lb from "./lb.svg";
+import * as lc from "./lc.svg";
+import * as li from "./li.svg";
+import * as lk from "./lk.svg";
+import * as lr from "./lr.svg";
+import * as ls from "./ls.svg";
+import * as lt from "./lt.svg";
+import * as lu from "./lu.svg";
+import * as lv from "./lv.svg";
+import * as ly from "./ly.svg";
+import * as ma from "./ma.svg";
+import * as mc from "./mc.svg";
+import * as md from "./md.svg";
+import * as me from "./me.svg";
+import * as mf from "./mf.svg";
+import * as mg from "./mg.svg";
+import * as mh from "./mh.svg";
+import * as mk from "./mk.svg";
+import * as ml from "./ml.svg";
+import * as mm from "./mm.svg";
+import * as mn from "./mn.svg";
+import * as mo from "./mo.svg";
+import * as mp from "./mp.svg";
+import * as mq from "./mq.svg";
+import * as mr from "./mr.svg";
+import * as ms from "./ms.svg";
+import * as mt from "./mt.svg";
+import * as mu from "./mu.svg";
+import * as mv from "./mv.svg";
+import * as mw from "./mw.svg";
+import * as mx from "./mx.svg";
+import * as my from "./my.svg";
+import * as mz from "./mz.svg";
+import * as na from "./na.svg";
+import * as nc from "./nc.svg";
+import * as ne from "./ne.svg";
+import * as nf from "./nf.svg";
+import * as ng from "./ng.svg";
+import * as ni from "./ni.svg";
+import * as nl from "./nl.svg";
+import * as no from "./no.svg";
+import * as np from "./np.svg";
+import * as nr from "./nr.svg";
+import * as nu from "./nu.svg";
+import * as nz from "./nz.svg";
+import * as om from "./om.svg";
+import * as pa from "./pa.svg";
+import * as pe from "./pe.svg";
+import * as pf from "./pf.svg";
+import * as pg from "./pg.svg";
+import * as ph from "./ph.svg";
+import * as pk from "./pk.svg";
+import * as pl from "./pl.svg";
+import * as pm from "./pm.svg";
+import * as pn from "./pn.svg";
+import * as pr from "./pr.svg";
+import * as pt from "./pt.svg";
+import * as pw from "./pw.svg";
+import * as py from "./py.svg";
+import * as qa from "./qa.svg";
+import * as re from "./re.svg";
+import * as ro from "./ro.svg";
+import * as rs from "./rs.svg";
+import * as ru from "./ru.svg";
+import * as rw from "./rw.svg";
+import * as sa from "./sa.svg";
+import * as sb from "./sb.svg";
+import * as sc from "./sc.svg";
+import * as sd from "./sd.svg";
+import * as se from "./se.svg";
+import * as sg from "./sg.svg";
+import * as sh from "./sh.svg";
+import * as si from "./si.svg";
+import * as sj from "./sj.svg";
+import * as sk from "./sk.svg";
+import * as sl from "./sl.svg";
+import * as sm from "./sm.svg";
+import * as sn from "./sn.svg";
+import * as so from "./so.svg";
+import * as sr from "./sr.svg";
+import * as ss from "./ss.svg";
+import * as st from "./st.svg";
+import * as sv from "./sv.svg";
+import * as sx from "./sx.svg";
+import * as sy from "./sy.svg";
+import * as sz from "./sz.svg";
+import * as tc from "./tc.svg";
+import * as td from "./td.svg";
+import * as tf from "./tf.svg";
+import * as tg from "./tg.svg";
+import * as th from "./th.svg";
+import * as tj from "./tj.svg";
+import * as tk from "./tk.svg";
+import * as tl from "./tl.svg";
+import * as tm from "./tm.svg";
+import * as tn from "./tn.svg";
+import * as to from "./to.svg";
+import * as tr from "./tr.svg";
+import * as tt from "./tt.svg";
+import * as tv from "./tv.svg";
+import * as tw from "./tw.svg";
+import * as tz from "./tz.svg";
+import * as ua from "./ua.svg";
+import * as ug from "./ug.svg";
+import * as um from "./um.svg";
+import * as us from "./us.svg";
+import * as uy from "./uy.svg";
+import * as uz from "./uz.svg";
+import * as va from "./va.svg";
+import * as vc from "./vc.svg";
+import * as ve from "./ve.svg";
+import * as vg from "./vg.svg";
+import * as vi from "./vi.svg";
+import * as vn from "./vn.svg";
+import * as vu from "./vu.svg";
+import * as wf from "./wf.svg";
+import * as ws from "./ws.svg";
+import * as ye from "./ye.svg";
+import * as ytunf from "./yt-unf.svg";
+import * as za from "./za.svg";
+import * as zm from "./zm.svg";
+
+export const ICONS = {
+  ad,
+  ae,
+  af,
+  ag,
+  ai,
+  al,
+  am,
+  ao,
+  aq,
+  ar,
+  as,
+  at,
+  au,
+  aw,
+  ax,
+  az,
+  ba,
+  bb,
+  bd,
+  be,
+  bf,
+  bg,
+  bh,
+  bi,
+  bj,
+  bl,
+  bm,
+  bn,
+  bo,
+  'bq-bo': bqbo,
+  sa,
+  se,
+  br,
+  bs,
+  bt,
+  bv,
+  bw,
+  by,
+  bz,
+  ca,
+  cc,
+  cd,
+  cf,
+  cg,
+  ch,
+  ci,
+  ck,
+  cl,
+  cm,
+  cn,
+  co,
+  cr,
+  cu,
+  cv,
+  cw,
+  cx,
+  cy,
+  cz,
+  de,
+  dj,
+  dk,
+  dm,
+  do: DO,
+  dz,
+  ec,
+  ee,
+  eg,
+  eh,
+  er,
+  es,
+  et,
+  fi,
+  fj,
+  fk,
+  fm,
+  fo,
+  fr,
+  ga,
+  'gb-eng': gbeng,
+  'gb-nir': gbnir,
+  'gb-sct': gbsct,
+  'gb-ukm': gbukm,
+  'gb-wls': gbwls,
+  gd,
+  ge,
+  gf,
+  gg,
+  gh,
+  gi,
+  gl,
+  gm,
+  gn,
+  gp,
+  gq,
+  gr,
+  gs,
+  gt,
+  gu,
+  gw,
+  gy,
+  hk,
+  hm,
+  hn,
+  hr,
+  ht,
+  hu,
+  id,
+  ie,
+  il,
+  im,
+  inFlag,
+  io,
+  iq,
+  ir,
+  is,
+  it,
+  je,
+  jm,
+  jo,
+  jp,
+  ke,
+  kg,
+  kh,
+  ki,
+  km,
+  kn,
+  kp,
+  kr,
+  kw,
+  ky,
+  kz,
+  la,
+  lb,
+  lc,
+  li,
+  lk,
+  lr,
+  ls,
+  lt,
+  lu,
+  lv,
+  ly,
+  ma,
+  mc,
+  md,
+  me,
+  mf,
+  mg,
+  mh,
+  mk,
+  ml,
+  mm,
+  mn,
+  mo,
+  mp,
+  mq,
+  mr,
+  ms,
+  mt,
+  mu,
+  mv,
+  mw,
+  mx,
+  my,
+  mz,
+  na,
+  nc,
+  ne,
+  nf,
+  ng,
+  ni,
+  nl,
+  no,
+  np,
+  nr,
+  nu,
+  nz,
+  om,
+  pa,
+  pe,
+  pf,
+  pg,
+  ph,
+  pk,
+  pl,
+  pm,
+  pn,
+  pr,
+  pt,
+  pw,
+  py,
+  qa,
+  re,
+  ro,
+  rs,
+  ru,
+  rw,
+  'bq-sa': bqsa,
+  sb,
+  sc,
+  sd,
+  'bq-se': bqse,
+  sg,
+  sh,
+  si,
+  sj,
+  sk,
+  sl,
+  sm,
+  sn,
+  so,
+  sr,
+  ss,
+  st,
+  sv,
+  sx,
+  sy,
+  sz,
+  tc,
+  td,
+  tf,
+  tg,
+  th,
+  tj,
+  tk,
+  tl,
+  tm,
+  tn,
+  to,
+  tr,
+  tt,
+  tv,
+  tw,
+  tz,
+  ua,
+  ug,
+  um,
+  us,
+  uy,
+  uz,
+  va,
+  vc,
+  ve,
+  vg,
+  vi,
+  vn,
+  vu,
+  wf,
+  ws,
+  ye,
+  'yt-unf': ytunf,
+  za,
+  zm,
+  bq,
+  gb,
+};
